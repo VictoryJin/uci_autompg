003a151445bb944406f488a64e04c706a6cc57d1
Victor Jin <yoolim99@berkeley.edu>
2021-07-08 00:09:05 -0700

    Added recursive implementation of levenshtein distance

--- src/utils.py
+++ src/utils.py
@@ -26,3 +26,40 @@ def import_dataset(offline=True):
 
     df['origin'] = df['origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
     return df
+
+
+def lazy_ldist(word1, word2):
+    """Recomputes the distance for recursively via insert, delete, replace
+    Reference: https://en.wikipedia.org/wiki/Levenshtein_distance#Recursive
+    
+    >>> lazy_ldist('test_insertion', 'test_insertion_extra')
+    6
+    >>> lazy_ldist('test_deletion', 'test')
+    9
+    >>> lazy_ldist('asdf', 'dsdf')
+    1
+    >>> lazy_ldist('test_replacement', 'xesd_replacement')
+    # Should be 2, but takes infinite due insertion
+    
+    Parameters:
+        wd1, wd2 (str): first and second string
+    """
+    assert isinstance(word1, str) & isinstance(word2, str)
+    
+    # Always enforce 
+    
+    lword1, lword2 = len(word1), len(word2) 
+    # Base case 1: if either word is empty then distance is the number of characters in t
+    if lword1==0:
+        return lword2
+    if lword2==0:
+        return lword1
+    
+    # Base case 2: if characters are the same, ignore
+    if word1[0]==word2[0]:
+        return lazy_ldist(word1[1:], word2[1:])
+    
+    # If not, recursively try insertion, deletion, and substitution
+    return 1 + min(lazy_ldist(word2[0]+word1, word2),     # insertion
+                   lazy_ldist(word1[1:], word2),          # deletion
+                   lazy_ldist(word2[0]+word1[1:], word2)) # replacement
